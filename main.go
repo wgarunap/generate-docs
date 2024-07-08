49300120312125ded8c5c46c78b93d496f3d64c0
agunasek <agunasek@xin722827.fritz.box>
2024-07-08 23:41:22 +0200

    link fixes

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func init() {
 func main() {
 
 	htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
-		CDN:     "https://github.com/wgarunap/generate-docs/blob/master/static/script/api-reference.js",
+		CDN:     "https://raw.githubusercontent.com/wgarunap/generate-docs/master/static/script/api-reference.js",
 		SpecURL: filepath,
 		CustomOptions: scalar.CustomOptions{
 			PageTitle: "API Documentation",
